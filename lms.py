44a46935a5a2f7d766814619c1ebc61984f2ec0a
upne <iambz00@gmail.com>
2023-04-25 14:50:04 +0900

    Initial commit

---
+++ lms.py
@@ -0,0 +1,207 @@
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+#from selenium.common.exceptions import UnexpectedAlertPresentException
+#from selenium.common.exceptions import JavascriptException
+#from selenium.common.exceptions import NoSuchElementException
+import selenium.common.exceptions as SeleniumException
+from wcwidth import wcswidth
+
+import time, os, sys
+import getpass
+import urllib3
+
+version = "20230328"
+
+# Fill blank
+def p(*args, **kwargs):
+    width = os.get_terminal_size().columns
+    length = 0
+    for i in range(len(args)):
+        length += wcswidth(args[i].replace('\r', ''))
+    length += len(args) - 1
+    print(*args, " " * (width-length-2), **kwargs)
+
+class Lms:
+    def __init__(self, url = "https://www.gbeti.or.kr"):
+        os.environ["WDM_SSL_VERIFY"] = "0"
+        urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+        options = webdriver.ChromeOptions()
+        #options.add_argument("--log-level=2")
+        im_not_headless = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36"
+        options.add_argument("user-agent=" + im_not_headless)
+        options.add_argument("--headless")
+        options.add_argument("--window-size=1920,1080")
+        options.add_argument("--disable-gpu")
+        options.add_experimental_option('excludeSwitches', ['enable-logging'])
+        self.driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
+        self.driver.implicitly_wait(3)
+        self.hMain = self.driver.current_window_handle
+        self.delay = 1
+        self.wait = WebDriverWait(self.driver, 10)
+        self.url = url
+        self.courseList = []
+    def __del__(self):
+        self.close()
+    def close_popups(self):
+        while len(self.driver.window_handles) > 1:
+            for handle in self.driver.window_handles:
+                if handle != self.hMain:
+                    self.driver.switch_to.window(handle)
+                    self.driver.close()
+        self.driver.switch_to.window(self.hMain)
+    def login(self):
+        print("\n* 로그인")
+        userID = input("  ID: ")
+        userPW = getpass.getpass("  PW: ")
+        self.driver.get(self.url + "/system/login/login.do")
+        self.close_popups()
+
+        login_id = self.driver.find_element(By.CSS_SELECTOR, "#userInputId")
+        login_id.click()
+        
+        webdriver.ActionChains(self.driver)\
+            .pause(1)\
+            .send_keys(userID)\
+            .send_keys(Keys.TAB)\
+            .pause(1)\
+            .send_keys(userPW)\
+            .pause(1)\
+            .send_keys(Keys.ENTER)\
+            .pause(3)\
+            .perform()
+        try:
+            is_login_complete = self.driver.current_url.find("login") > 0
+        except SeleniumException.UnexpectedAlertPresentException as e:
+            is_login_complete = self.driver.current_url.find("login") > 0
+            print("\n! 로그인 오류")
+            self.login()
+        else:
+            self.close_popups()
+    def select_course(self):
+        print("\n* 과정 선택")
+        if not self.courseList:
+            print("  페이지 로딩 중...", end="")
+            sys.stdout.flush()
+            self.driver.get(self.url + "/lh/ms/cs/atnlcListView.do?menuId=3000000101")
+
+            self.driver.find_elements(By.CSS_SELECTOR, "#crseList > li")
+            courses = self.driver.find_elements(By.CSS_SELECTOR, "#crseList > li")
+            p("\r")
+
+            for i in range(len(courses)):
+                text = courses[i].find_element(By.CSS_SELECTOR, "a.title").text
+                if len(text) > 40:
+                    text = text[:36] + "..."
+                button = ""
+                for abtn in courses[i].find_elements(By.CSS_SELECTOR, "a"):
+                    if abtn.text == "이어보기" or abtn.text == "학습하기":
+                        button = abtn
+                        break
+                self.courseList.append({ 'text': f"  [{i+1}] {text}", 'obj': button})
+        for i in range(len(self.courseList)):
+            print(self.courseList[i]['text'])
+        print("  [0] 종료하기")
+        num = int(input("\n  과정을 선택하세요 [1]: ") or "1") - 1
+        if num == -1:
+            self.close()
+        else:
+            if num not in range(len(self.courseList)):
+                num = 0
+            num_windows = len(self.driver.window_handles)
+            self.courseList[num]['obj'].click()
+            time.sleep(2)
+            self.hLearn = self.get_new_window(num_windows)
+            self.driver.switch_to.window(self.hLearn)
+            self.learn()
+    def get_new_window(self, num_windows_before):
+        self.wait.until(EC.number_of_windows_to_be(num_windows_before + 1))
+        return self.driver.window_handles[-1]
+    def return_to_main(self):
+        self.driver.switch_to.window(self.hMain)
+    def learn(self):
+        print("\n* 수강 시작")
+        current_subject = " "
+        current_section = " "
+        current_subsect = " "
+        current_progress = 0.0
+        startbutton = self.driver.find_element(By.CSS_SELECTOR, 'div#lectBtnControl')
+        if startbutton.is_displayed():
+            startbutton.click()
+            time.sleep(2)
+        try:
+            while True:
+                time.sleep(self.delay)
+                subject = self.driver.find_element(By.CSS_SELECTOR, "div.class_list p.title_box").text.strip()
+                section = self.driver.find_element(By.CSS_SELECTOR, "div.class_list_box.ing li.play div a").text.strip()
+                if not section or section == "학습하기":
+                    section = self.driver.find_element(By.CSS_SELECTOR, "div.class_list_box.ing p").text.strip()
+                subsect = self.driver.find_element(By.CSS_SELECTOR, "#page-info").text.strip()
+                if subject and subject != current_subject:
+                    p(f"\r[차시]: {subject}")
+                    current_subject = subject
+                elif section and section != current_section:
+                    p(f"\r  [강의]: {section} [{subsect}]")
+                    current_section = section
+                    current_subsect = subsect
+                elif subsect and subsect != current_subsect:
+                    p(f"\r  [강의]: {section} [{subsect}]")
+                    current_subsect = subsect
+                progress = float(self.driver.find_element(By.CSS_SELECTOR, "#lx-player div.vjs-progress-holder").get_attribute("aria-valuenow"))
+                progress_time = self.driver.find_element(By.CSS_SELECTOR, "#lx-player div.vjs-progress-holder").get_attribute("aria-valuetext").strip().split()
+                is_quizpage = self.driver.find_element(By.CSS_SELECTOR, "#quizPage").is_displayed()
+                if len(progress_time) > 0:
+                    played = progress_time[0]
+                    length = progress_time[2]
+                playbutton = self.driver.find_element(By.CSS_SELECTOR, "button.vjs-big-play-button")
+                if progress == current_progress:
+                    if is_quizpage:
+                        p(f"\r    [퀴즈]")
+                        current_progress = 100.0
+                    elif played == length:
+                        current_progress = 100.0
+                        time.sleep(1)
+                    elif playbutton.is_displayed():
+                        p(f"\r! 재생 시작")
+                        playbutton.click()
+                else:
+                    current_progress = progress
+                self.print_progress(current_progress, played, length)
+                if current_progress >= 100.0:
+                    self.driver.execute_script("next_ScoBtn()")
+        except SeleniumException.UnexpectedAlertPresentException as e:
+            print("\n* 경고 - " + e.alert_text)
+        except SeleniumException.JavascriptException as e:
+            print("\n* 수강 완료")
+        except Exception as e:
+            print("\n* 오류 발생 - " + str(e))
+        except KeyboardInterrupt:
+            print("\n* 종료합니다.\n")
+            time.sleep(2)
+        finally:
+            if self.driver.service.is_connectable():
+                self.driver.close()
+                self.return_to_main()
+                self.select_course()
+            else:
+                sys.exit(1)
+    def print_progress(self, progress, played = "", length = ""):
+        strLength = min(50, os.get_terminal_size().columns - 30)
+        fillLength = int(round(strLength * progress / 100))
+        filledStr = "-" * fillLength + " " * (strLength - fillLength)
+        print(f"\r    [{played:>5} / {length:>5}] [{filledStr}] {progress:05.2f}%", end="", flush=True)
+    def close(self):
+        print("\n* 종료합니다.")
+        if self.driver.service.is_connectable():
+            self.driver.quit()
+        sys.exit(0)
+
+if __name__ == "__main__":
+    print(f"\n  연수원 강의 듣기 v{version}")
+    lms = Lms()
+    lms.login()
+    lms.select_course()
